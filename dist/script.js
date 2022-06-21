0c6f0824edf308228447778072696de6176a9e22
olga <yaolmi@yandex.ru>
2022-06-21 11:10:59 +0700

    create & stylize portfolio section

---
+++ dist/script.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+});
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
