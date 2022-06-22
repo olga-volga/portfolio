eec1167ac0d1186ad513f192dbc2b79c20f18552
olga <yaolmi@yandex.ru>
2022-06-22 19:58:42 +0700

    add scroll calculation for display modal

--- dist/script.js
+++ dist/script.js
@@ -17,6 +17,29 @@ const modalImage = () => {
         images = document.querySelectorAll('.portfolio__img'),
         modal = document.querySelector('.modal'),
         close = document.querySelector('.modal__close');
+
+  function calcScroll() {
+    const div = document.createElement('div');
+    div.style.cssText = 'width:50px;height:50px;overflow-y:scroll;visibilty:hidden;';
+    document.body.append(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    return scrollWidth;
+  }
+
+  function showModal() {
+    modal.style.display = 'flex';
+    document.body.style.paddingRight = `${calcScroll()}px`;
+    document.body.style.overflow = 'hidden';
+  }
+
+  function closeModal() {
+    modal.style.display = '';
+    document.body.style.paddingRight = '';
+    document.body.style.overflow = '';
+    modal.removeChild(modal.lastElementChild);
+  }
+
   imagesParent.addEventListener('click', e => {
     if (e.target && e.target.nodeName === 'IMG') {
       images.forEach(item => {
@@ -27,17 +50,14 @@ const modalImage = () => {
           imageBig.setAttribute('src', srcPath);
           imageBig.classList.add('appear');
           modal.append(imageBig);
-          modal.style.display = 'flex';
-          document.body.style.overflow = 'hidden';
+          showModal();
         }
       });
     }
   });
   modal.addEventListener('click', e => {
     if (e.target === modal || e.target === close && modal.style.display !== 'none') {
-      modal.style.display = '';
-      modal.removeChild(modal.lastElementChild);
-      document.body.style.overflow = '';
+      closeModal();
     }
   });
 };
